8954a2c64aae77d034d4e88fcda9c54084b7c6a7
julian <juliii@gmail.com>
2012-01-20 15:12:46 +0100

    fixed utorrent response handling

--- webuiapis/uTorrentWebUI.js
+++ webuiapis/uTorrentWebUI.js
@@ -1,3 +1,15 @@
+function handleResponse(data) {
+	if(this.readyState == 4 && this.status == 200) {
+		if(/\{"build":\d+?\}/.test(this.responseText)) {
+			displayResponse("Success", "Torrent added successfully.");
+		} else {
+			displayResponse("Failure", "Server didn't accept data:\n"+this.status+": "+this.responseText);
+		}
+	} else if(this.readyState == 4 && this.status != 200) {
+		displayResponse("Failure", "Server responded with an irregular HTTP error code:\n"+this.status+": "+this.responseText);
+	}
+};
+
 function addTorrentTouTorrentWebUI(torrentdata) {
 	var xhr = new XMLHttpRequest();
 	xhr.open("GET", "http://"+localStorage["host"]+":"+localStorage["port"]+"/gui/token.html", false, localStorage["login"], localStorage["password"]);
@@ -7,26 +19,23 @@ function addTorrentTouTorrentWebUI(torrentdata) {
 	var xhr = new XMLHttpRequest();
 	
 	xhr.open("POST", "http://"+localStorage["host"]+":"+localStorage["port"]+"/gui/?token="+token+"&action=add-file", true, localStorage["login"], localStorage["password"]);
-	xhr.onreadystatechange = function(data) {
-		if(xhr.readyState == 4 && xhr.status == 200) {
-			if(/\{"build":\d+?\}/.test(xhr.responseText)) {
-				displayResponse("Success", "Torrent added successfully.");
-			} else {
-				displayResponse("Failure", "Server didn't accept data:\n"+xhr.status+": "+xhr.responseText);
-			}
-		} else if(xhr.readyState == 4 && xhr.status != 200) {
-			displayResponse("Failure", "Server responded with an irregular HTTP error code:\n"+xhr.status+": "+xhr.responseText);
-		}
-	};
+	xhr.onreadystatechange = handleResponse;
 	
-	// mostly stolen from https://github.com/igstan/ajax-file-upload/blob/master/complex/uploader.js
-	var boundary = "AJAX-----------------------"+(new Date).getTime();
-	xhr.setRequestHeader("Content-Type", "multipart/form-data; boundary=" + boundary);
-	var message = "--" + boundary + "\r\n";
-	   message += "Content-Disposition: form-data; name=\"torrent_file\"; filename=\"file.torrent\"\r\n";
-	   message += "Content-Type: application/x-bittorrent\r\n\r\n";
-	   message += torrentdata + "\r\n";
-	   message += "--" + boundary + "--\r\n";
-	
-	xhr.sendAsBinary(message);
+	if(torrentdata.substring(0,7) == "magnet:") {
+		var mxhr = new XMLHttpRequest();
+		mxhr.open("GET", "http://"+localStorage["host"]+":"+localStorage["port"]+"/gui/?token="+token+"&action=add-url&s="+encodeURIComponent(torrentdata), true, localStorage["login"], localStorage["password"]);
+		mxhr.onreadystatechange = handleResponse;
+		mxhr.send(message);
+	} else {
+		// mostly stolen from https://github.com/igstan/ajax-file-upload/blob/master/complex/uploader.js
+		var boundary = "AJAX-----------------------"+(new Date).getTime();
+		xhr.setRequestHeader("Content-Type", "multipart/form-data; boundary=" + boundary);
+		var message = "--" + boundary + "\r\n";
+		   message += "Content-Disposition: form-data; name=\"torrent_file\"; filename=\"file.torrent\"\r\n";
+		   message += "Content-Type: application/x-bittorrent\r\n\r\n";
+		   message += torrentdata + "\r\n";
+		   message += "--" + boundary + "--\r\n";
+		
+		xhr.sendAsBinary(message);
+	}
 }
