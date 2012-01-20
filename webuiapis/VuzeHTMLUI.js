488195360d7b173b746d9d60a5cd8b04bf733f9d
julian <juliii@gmail.com>
2012-01-20 15:18:21 +0100

    fixed vuze html ui response handling

--- webuiapis/VuzeHTMLUI.js
+++ webuiapis/VuzeHTMLUI.js
@@ -1,22 +1,24 @@
+function handleResponse(data) {
+	if(this.readyState == 4 && this.status == 200) {
+		if(/.*loaded successfully.*/.exec(this.responseText)) {
+			displayResponse("Success", "Torrent added successfully.");
+		} else {
+			displayResponse("Failure", "Server didn't accept data:\n"+this.status+": "+this.responseText);
+		}
+	} else if(this.readyState == 4 && this.status != 200) {
+		displayResponse("Failure", "Server responded with an irregular HTTP error code:\n"+this.status+": "+this.responseText);
+	}
+};
+
 function addTorrentToVuzeHTMLUI(data) {
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", "http://"+localStorage["host"]+":"+localStorage["port"]+"/index.tmpl?d=u&local=1", true, localStorage["login"], localStorage["password"]);
-	xhr.onreadystatechange = function(data) {
-		if(xhr.readyState == 4 && xhr.status == 200) {
-			if(/.*loaded successfully.*/.exec(xhr.responseText)) {
-				displayResponse("Success", "Torrent added successfully.");
-			} else {
-				displayResponse("Failure", "Server didn't accept data:\n"+xhr.status+": "+xhr.responseText);
-			}
-		} else if(xhr.readyState == 4 && xhr.status != 200) {
-			displayResponse("Failure", "Server responded with an irregular HTTP error code:\n"+xhr.status+": "+xhr.responseText);
-		}
-	};
+	xhr.onreadystatechange = handleResponse;
 	
 	if(data.substring(0,7) == "magnet:") {
 		var mxhr = new XMLHttpRequest();
 		mxhr.open("GET", "http://"+localStorage["host"]+":"+localStorage["port"]+"/index.tmpl?d=u&upurl="+encodeURIComponent(data), true, localStorage["login"], localStorage["password"]);
-		mxhr.onreadystatechange = xhr.onreadystatechange;
+		mxhr.onreadystatechange = handleResponse;
 		mxhr.send(message);
 	} else {
 		// mostly stolen from https://github.com/igstan/ajax-file-upload/blob/master/complex/uploader.js
