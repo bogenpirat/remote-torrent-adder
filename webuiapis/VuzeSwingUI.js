6aa972610a88043b0463e62059870cf32beceeb7
julian <juliii@gmail.com>
2012-01-20 17:10:25 +0100

    moved around some code in vuze swing ui module

--- webuiapis/VuzeSwingUI.js
+++ webuiapis/VuzeSwingUI.js
@@ -1,4 +1,9 @@
 function addTorrentToVuzeSwingUI(data) {
+	if(data.substring(0,7) == "magnet:") {
+		displayResponse("Client Failure", "sorry, no magnet/link adding support from vuze swing ui. try the vuze remote plugin.");
+		return;
+	}
+	
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", "http://"+localStorage["host"]+":"+localStorage["port"]+"/upload.cgi", true, localStorage["login"], localStorage["password"]);
 	xhr.onreadystatechange = function(data) {
@@ -13,18 +18,14 @@ function addTorrentToVuzeSwingUI(data) {
 		}
 	};
 	
-	if(data.substring(0,7) == "magnet:") {
-		displayResponse("Client Failure", "sorry, no magnet/link adding support from vuze swing ui. try the vuze remote plugin.");
-	} else {
-		// mostly stolen from https://github.com/igstan/ajax-file-upload/blob/master/complex/uploader.js
-		var boundary = "AJAX-----------------------"+(new Date).getTime();
-		xhr.setRequestHeader("Content-Type", "multipart/form-data; boundary=" + boundary);
-		var message = "--" + boundary + "\r\n";
-		   message += "Content-Disposition: form-data; name=\"upfile\"; filename=\"file.torrent\"\r\n";
-		   message += "Content-Type: application/x-bittorrent\r\n\r\n";
-		   message += data + "\r\n";
-		   message += "--" + boundary + "--\r\n";
-		
-		xhr.sendAsBinary(message);
-	}
+	// mostly stolen from https://github.com/igstan/ajax-file-upload/blob/master/complex/uploader.js
+	var boundary = "AJAX-----------------------"+(new Date).getTime();
+	xhr.setRequestHeader("Content-Type", "multipart/form-data; boundary=" + boundary);
+	var message = "--" + boundary + "\r\n";
+	   message += "Content-Disposition: form-data; name=\"upfile\"; filename=\"file.torrent\"\r\n";
+	   message += "Content-Type: application/x-bittorrent\r\n\r\n";
+	   message += data + "\r\n";
+	   message += "--" + boundary + "--\r\n";
+	
+	xhr.sendAsBinary(message);
 }
