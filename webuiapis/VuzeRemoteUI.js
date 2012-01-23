079351bc1b0f0a40ddb8b1a367a27d695757d551
julian <juliii@gmail.com>
2012-01-23 13:40:01 +0100

    fixed vuze remote ui magnet adding

--- webuiapis/VuzeRemoteUI.js
+++ webuiapis/VuzeRemoteUI.js
@@ -1,9 +1,12 @@
 function addTorrentToVuzeRemoteUI(data) {
+	if(data.substring(0,7) == "magnet:") target = "rpc";
+	else target = "upload?paused=false";
+	
 	var xhr = new XMLHttpRequest();
-	xhr.open("POST", "http"+((localStorage["hostsecure"]=='true')?"s":"")+"://"+localStorage["host"]+":"+localStorage["port"]+"/transmission/upload?paused=false", true, localStorage["login"], localStorage["password"]);
+	xhr.open("POST", "http"+((localStorage["hostsecure"]=='true')?"s":"")+"://"+localStorage["host"]+":"+localStorage["port"]+"/transmission/"+target, true, localStorage["login"], localStorage["password"]);
 	xhr.onreadystatechange = function(data) {
 		if(xhr.readyState == 4 && xhr.status == 200) {
-			if(/.*<h1>200: OK<\/h1>.*/.exec(xhr.responseText)) {
+			if(/.*<h1>200: OK<\/h1>.*/.exec(xhr.responseText) || JSON.parse(xhr.responseText)["result"] == "success") {
 				displayResponse("Success", "Torrent added successfully.");
 			} else {
 				displayResponse("Failure", "Server didn't accept data:\n"+xhr.status+": "+xhr.responseText);
@@ -17,9 +20,9 @@ function addTorrentToVuzeRemoteUI(data) {
 		var message = JSON.stringify({"method":"torrent-add", "arguments":{"paused":"false", "filename": data}});
 		xhr.send(message);
 	} else {
-		// mostly stolen from https://github.com/igstan/ajax-file-upload/blob/master/complex/uploader.js
 		var boundary = "AJAX-----------------------"+(new Date).getTime();
 		xhr.setRequestHeader("Content-Type", "multipart/form-data; boundary=" + boundary);
+		// mostly stolen from https://github.com/igstan/ajax-file-upload/blob/master/complex/uploader.js
 		var message = "--" + boundary + "\r\n";
 		    message += "Content-Disposition: form-data; name=\"torrent_files[]\"; filename=\"file.torrent\"\r\n";
 		    message += "Content-Type: application/x-bittorrent\r\n\r\n";
