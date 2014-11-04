a3bce4d19b1db66012ff10234323937db776acb5
julian <juliii@gmail.com>
2014-11-04 03:32:14 +0100

    fixed ordering of servers saving

---
+++ miscapis/functions.js
@@ -0,0 +1,144 @@
+XMLHttpRequest.prototype.sendAsBinary = function(datastr) {
+	var data = new ArrayBuffer(datastr.length);
+	var ui8a = new Uint8Array(data, 0);
+	for (var i=0; i<datastr.length; i++) {
+		ui8a[i] = (datastr.charCodeAt(i) & 0xff);
+	}
+	var blob = new Blob([data]);
+	this.send(blob);
+}
+
+
+RTA.dispatchTorrent = function(server, data, name, label, dir) {
+	switch (server.client) {
+		case "Vuze SwingUI":
+			RTA.clients.vuzeSwingAdder(server, data); break;
+		case "Torrentflux WebUI":
+			RTA.clients.torrentfluxAdder(server, data, name); break;
+		case "Transmission WebUI":
+			RTA.clients.transmissionAdder(server, data); break;
+		case "uTorrent WebUI":
+			RTA.clients.uTorrentAdder(server, data); break;
+		case "ruTorrent WebUI":
+			RTA.clients.ruTorrentAdder(server, data, label, dir); break;
+		case "Vuze HTML WebUI":
+			RTA.clients.vuzeHtmlAdder(server, data); break;
+		case "Vuze Remote WebUI":
+			RTA.clients.vuzeRemoteAdder(server, data); break;
+		case "Buffalo WebUI":
+		case "Buffalo WebUI (OLD!)":
+			RTA.clients.buffaloAdder(server, data, name); break;
+		case "qBittorrent WebUI":
+			RTA.clients.qBittorrentAdder(server, data, name); break;
+		case "Deluge WebUI":
+			RTA.clients.delugeAdder(server, data, name); break;
+		case "pyrt WebUI":
+			RTA.clients.pyrtAdder(server, data, name); break;
+		case "Tixati WebUI":
+			RTA.clients.tixatiAdder(server, data, name); break;
+	}
+}
+
+
+RTA.getTorrent = function(server, url, label, dir) {
+	if(url.substring(0,7) == "magnet:") {
+		RTA.dispatchTorrent(server, url, "", label, dir);
+	} else {
+		var xhr = new XMLHttpRequest();
+		xhr.open("GET", url, true);
+		xhr.overrideMimeType("text/plain; charset=x-user-defined");
+		xhr.onreadystatechange = function(data) {
+			if(xhr.readyState == 4 && xhr.status == 200) {
+				if(url.match(/\/([^\/]+.torrent)$/)) {
+					name = url.match(/\/([^\/]+.torrent)$/)[1];
+				} else {
+					name = "file.torrent";
+				}
+				
+				RTA.dispatchTorrent(server, xhr.responseText, name, label, dir);
+			} else if(xhr.readyState == 4 && xhr.status < 99) {
+				RTA.displayResponse("Connection failed", "The server sent an irregular HTTP error code: " + xhr.status, true);
+			} else if(xhr.readyState == 4 && xhr.status != 200) {
+				RTA.displayResponse("Connection failed", "The server sent the following HTTP error code: " + xhr.status, true);
+			}
+		};
+		xhr.send(null);
+	}
+}
+
+
+RTA.displayResponse = function(title, message, error) {
+	if(localStorage.getItem("showpopups") == "true") {
+		var opts = { 
+					type: "basic", 
+					iconUrl: (error === true) ? "icons/BitTorrent128-red.png" : "icons/BitTorrent128.png", 
+					title: title,
+					priority: 0,
+					message: message
+					};
+		var id = Math.floor(Math.random() * 99999) + "";
+		
+		chrome.notifications.create(id, opts, function(myId) { id = myId });
+		
+		setTimeout(function(){chrome.notifications.clear(id, function() {});}, localStorage.getItem('popupduration'));
+	}
+}
+
+
+RTA.constructContextMenu = function() {
+	chrome.contextMenus.removeAll();
+
+	if(localStorage.getItem("catchfromcontextmenu") == "true") {
+		// for if there's only one entry
+		var contextMenuId = chrome.contextMenus.create({
+			"title": "Add to Remote WebUI",
+			"contexts": [ "link" ],
+			"onclick": RTA.genericOnClick
+		});
+		RTA.menuItemIndexToServerIndex[contextMenuId] = 0;
+
+		// check if there's more than one entry and add them as sub-entries in the context menu
+		var servers = localStorage.getItem("servers") ? JSON.parse(localStorage.getItem("servers")) : [];
+		var numServers = servers.length;
+
+		if(numServers > 1) {
+			for(var i = 0; i < numServers; i++) {
+				var thisId = chrome.contextMenus.create({
+					"title": servers[i].name,
+					"contexts": [ "link" ],
+					"parentId": contextMenuId,
+					"onclick": RTA.genericOnClick
+				});
+				RTA.menuItemIndexToServerIndex[thisId] = i;
+			}
+			chrome.contextMenus.create({"type" : "separator", "contexts": [ "link" ], "parentId": contextMenuId});
+			var allId = chrome.contextMenus.create({
+				"title": "send to all",
+				"contexts": [ "link" ],
+				"parentId": contextMenuId,
+				"onclick": RTA.genericOnClick
+			});
+			RTA.menuItemIndexToServerIndex[allId] = -1;
+		}
+	}
+}
+
+
+RTA.genericOnClick = function(info, tab) {
+	var servers = JSON.parse(localStorage.getItem("servers"));
+	var serverId = RTA.menuItemIndexToServerIndex[info.menuItemId];
+
+	if(serverId === -1) { // send to all servers
+		for(var i in servers) {
+			RTA.getTorrent(servers[i], info.linkUrl);
+		}
+	} else { // only one server specified
+		var server = JSON.parse(localStorage.getItem("servers"))[serverId];
+
+		if(server.rutorrentdirlabelask == true && server.client == "ruTorrent WebUI") {
+			chrome.tabs.sendRequest(tab.id, {"action": "showLabelDirChooser", "url": info.linkUrl, "settings": localStorage});
+		} else {
+			RTA.getTorrent(server, info.linkUrl);
+		}
+	}
+}
---
+++ miscapis/options.js
@@ -0,0 +1,296 @@
+$(document).ready(function(){
+	var tabCounter = 1;
+	
+	registerGeneralSettingsEvents();
+
+	loadGeneralSettings();
+	
+	$(function() {
+		$("#configtabs").tabs();
+	});
+
+	// new server type selection dialog // tab adding stuff
+	$(function() {
+		var tabTitle = $("#tab_title");
+		var tabClient = $("#tab_client");
+		var tabTemplate = "<li><a href='#{href}'>#{label}</a> <span class='ui-icon ui-icon-close' role='presentation'>Remove Tab</span></li>";
+		var tabs = $("#serverstabs").tabs();
+
+		tabs.find(".ui-tabs-nav").sortable({
+			axis: "x",
+			stop: function() {
+				tabs.tabs("refresh");
+			}
+		});
+
+		var dialog = $("#dialog").dialog({
+			autoOpen: false,
+			modal: true,
+			buttons: {
+				Add: function() {
+					if(addTab(tabTitle.val())) {
+						$(this).dialog("close");
+					}
+				},
+				Cancel: function() {
+					$(this).dialog("close");
+				}
+			},
+			close: function() {
+				form[0].reset();
+			}
+		});
+		
+		var form = dialog.find("form").submit(function() {
+			if(addTab(tabTitle.val())) {
+				dialog.dialog("close");
+			}
+			return false;
+		});
+
+		function addTab(name, client, oldload) {
+			// some input validation
+			var servers = JSON.parse(localStorage.getItem("servers"))
+			if(oldload !== true) {
+				for(var x in servers) {
+					if(servers[x].name == name || name === null || name === "") {
+						alert("This name is already in use by another server, or invalid.");
+						return false;
+					}
+				}
+			}
+
+			var id = "servertabs-" + tabCounter;
+			var li = $(tabTemplate.replace(/#\{href\}/g, "#" + id).replace(/#\{label\}/g, name));
+			var tabContentHtml = RTA.clients.config.getConfig(client || tabClient.val(), name);
+
+			tabs.find(".ui-tabs-nav").append(li);
+			tabs.append("<div id='" + id + "'><p>" + tabContentHtml + "</p></div>");
+			tabs.tabs("refresh");
+			if(oldload !== true) {
+				tabs.tabs("option", "active", -1);
+			} else {
+				tabs.tabs("option", "active", 0);
+			}
+			tabCounter++;
+
+			$("input").bind("change keyup", function(event) {
+				saveServersSettings();
+			});
+
+			return true;
+		}
+
+
+		$("#add_tab").button().click(function() {
+			dialog.dialog("open");
+		});
+
+		// close icon: removing the tab on click
+		tabs.delegate("span.ui-icon-close", "click", function() {
+			var panelId = $(this).closest("li").remove().attr("aria-controls");
+			$("#" + panelId).remove();
+			tabs.tabs("refresh");
+		});
+
+		tabs.bind("keyup", function(event) {
+			if (event.altKey && event.keyCode === $.ui.keyCode.BACKSPACE) {
+				var panelId = tabs.find(".ui-tabs-active").remove().attr("aria-controls");
+				$("#" + panelId).remove();
+				tabs.tabs("refresh");
+			}
+		});
+
+
+		// load server configs
+		var servers = JSON.parse(localStorage.getItem("servers"));
+		for(var i in servers) {
+			var server = servers[i];
+
+			addTab(server.name, server.client, true);
+
+			var mySettingInputs = $("#servertabs-" + (parseInt(i) + 1)).find("input").get();
+			for(var u in mySettingInputs) {
+				var mySettingInput = mySettingInputs[u];
+				if(server.hasOwnProperty(mySettingInput.name)) {
+					switch(mySettingInput.type) {
+						case "checkbox":
+							$(mySettingInput).prop('checked', server[mySettingInput.name]);
+							break;
+						default:
+							$(mySettingInput).val(server[mySettingInput.name]);
+					}
+				}
+			}
+		}
+	});
+});
+
+function loadGeneralSettings() {
+	var e = document.querySelectorAll("#linksfoundindicator,#showpopups,#popupduration,#catchfromcontextmenu,#catchfrompage,#linkmatches,#catchfromnewtab")
+	for (key in e) {
+		getSetting(e[key]);
+	}
+
+	// load matches
+	loadMatches();
+	
+	// set visibility
+	flipVisibility("showpopups", "popupduration");
+	flipVisibility("catchfrompage", "linkmatches");
+}
+
+function flipVisibility(checkname, changename) {
+	document.getElementById(changename).disabled = (document.getElementById(checkname).checked) ? false : true;
+}
+
+function setSetting(e, val) {
+	localStorage[e.id] = (val == undefined) ? "" : val;
+}
+
+function getSetting(e) {
+	if(e.type == "text" || e.type == "password") {
+		document.getElementById(e.id).value = (localStorage[e.id] == undefined) ? "" : localStorage[e.id];
+	} else if(e.type == "checkbox") {
+		document.getElementById(e.id).checked = (localStorage[e.id] == "true") ? true : false;
+	}
+}
+
+function saveMatches() {
+	var opts = document.getElementById("linkmatches").getElementsByTagName("option");
+	var destStr = ""; var i=0;
+	for(key in opts)
+		if(opts[key].text) {
+			var sep = (i++ == 0) ? "" : "~";
+			destStr += sep + opts[key].text;
+		}
+	localStorage["linkmatches"] = destStr;
+}
+
+function loadMatches() {
+	var newSelEl = document.createElement("select");
+	newSelEl.setAttribute("id", "linkmatches");
+	newSelEl.setAttribute("multiple", "multiple");
+	newSelEl.setAttribute("size", "5");
+	if(localStorage["linkmatches"] != "")
+		for(key in localStorage["linkmatches"].split("~")) {
+			var newEl = document.createElement("option");
+			newEl.text = localStorage["linkmatches"].split("~")[key];
+			newSelEl.appendChild(newEl);
+		}
+	var selEl = document.getElementById("linkmatches");
+	selEl.parentNode.appendChild(newSelEl);
+	selEl.parentNode.removeChild(selEl);
+}
+
+function addMatch() {
+	var newMatch = prompt("Enter a partial string of a link that should be caught by the extension","");
+	if(!newMatch) return;
+	
+	var newOpt = new Option(newMatch);
+	document.getElementById('linkmatches').appendChild(newOpt);
+	saveMatches();
+}
+
+function deleteMatches() {
+	var list = document.getElementById('linkmatches');
+	for(var i = list.length-1; i>=0; i--)
+		if(list.options[i].selected) {
+			list.removeChild(list.options[i]);
+		}
+	saveMatches();
+}
+
+Storage.prototype.setObject = function(key, val) {
+	this.setItem(key, JSON.stringify(val));
+}
+Storage.prototype.getObject = function(key) {
+	var value = this.getItem(key);
+    return value && JSON.parse(value);
+}
+
+function registerGeneralSettingsEvents() {
+	document.querySelector("#linksfoundindicator").onchange = function() {
+		setSetting(this, (this.checked) ? 'true' : 'false');
+	};
+	
+	document.querySelector("#showpopups").onchange = function() {
+		setSetting(this, (this.checked) ? 'true' : 'false');
+	};
+	document.querySelector("#showpopups").onclick = function() {
+		flipVisibility(this.id, 'popupduration');
+	};
+	
+	document.querySelector("#popupduration").onkeyup = function() {
+		setSetting(this, this.value);
+	};
+	
+	document.querySelector("#notificationtest").onclick = function() {
+		var opts = { 
+					type: "basic", 
+					iconUrl: "icons/BitTorrent128.png", 
+					title: "This is a test notification",
+					priority: 0,
+					message: "This is a test message!"
+					};
+		var id = Math.floor(Math.random() * 99999) + "";
+		
+		chrome.notifications.create(id, opts, function(myId) { id = myId });
+		
+		setTimeout(function(){chrome.notifications.clear(id, function() {});}, localStorage['popupduration']);
+	};
+	
+	document.querySelector("#catchfromcontextmenu").onchange = function() {
+		setSetting(this, (this.checked) ? 'true' : 'false');
+		console.debug("ret:"+RTA.constructContextMenu());
+	};
+	
+	document.querySelector("#catchfrompage").onchange = function() {
+		setSetting(this, (this.checked) ? 'true' : 'false');
+	};
+	document.querySelector("#catchfrompage").onclick = function() {
+		flipVisibility(this.id, 'linkmatches');
+	};
+	
+	document.querySelector("#catchfromnewtab").onchange = function() {
+		setSetting(this, (this.checked) ? 'true' : 'false');
+	};
+	
+	document.querySelector("#addfilterbtn").onclick = function() {
+		addMatch();
+	};
+	
+	document.querySelector("#delfilterbtn").onclick = function() {
+		deleteMatches();
+	};
+	
+	document.querySelector("#showfiltersbtn").onclick = function() {
+		alert(localStorage['linkmatches']);
+	};
+}
+
+function saveServersSettings() {
+	var servers = [];
+	$("div[id^=servertabs-]").each(function(i, el) {
+		var server = {};
+		var elements = $(el).find("input").get();
+		for(var u in elements) {
+			var element = elements[u];
+
+			switch(element.type) {
+				case "checkbox":
+					server[element.name] = $(element).prop('checked');
+					break;
+				default:
+					server[element.name] = $(element).val();
+			}
+		}
+		servers.push(server);
+	});
+
+	localStorage.setItem("servers", JSON.stringify(servers))
+
+	RTA.constructContextMenu();
+	
+	return servers;
+}
