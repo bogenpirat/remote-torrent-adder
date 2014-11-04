1a4edc9b3bd6243c157cce0aa19fb4933eeb519c
julian <juliii@gmail.com>
2014-11-04 02:42:20 +0100

    major refactoring, implemented multi-server support

--- webuiapis/pyrtWebUI.js
+++ webuiapis/pyrtWebUI.js
@@ -1,38 +1,38 @@
-function addTorrentTopyrtWebUI(data, filename) {
+RTA.clients.pyrtAdder = function(server, data, filename) {
 	if(data.substring(0,7) == "magnet:") {
 		displayResponse("Client Failure", "sorry, pyrt doesn't support magnet");
 		return;
 	}
 
-	var url = "http"+((localStorage["hostsecure"]=='true')?"s":"")+"://"+localStorage["host"]+":"+localStorage["port"]+"/";
+	var url = "http" + ((server.hostsecure == 'true') ? "s" : "") + "://" + server.host + ":" + server.port + "/";
 	// log in to create a functioning session
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", url, false);
 	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-	xhr.send("password="+localStorage["password"]);
+	xhr.send("password=" + server.password);
 	
 	if(/.*Incorrect Password.*/.exec(xhr.responseText)) {
-		displayResponse("Failure", "Credentials weren't accepted:\n"+xhr.responseText);
+		RTA.displayResponse("Failure", "Credentials weren't accepted:\n" + xhr.responseText);
 		return;
 	}
 	
 	// send the torrent
 	var xhr = new XMLHttpRequest();
-	xhr.open("POST", url+"ajax", true, localStorage["login"], localStorage["password"]);
+	xhr.open("POST", url + "ajax", true, server.login, server.password);
 	xhr.onreadystatechange = function(data) {
 		if(xhr.readyState == 4 && xhr.status == 200) {
 			if(/.*Redirect.*/.exec(xhr.responseText)) {
-				displayResponse("Success", "Torrent added successfully.");
+				RTA.displayResponse("Success", "Torrent added successfully.");
 			} else {
-				displayResponse("Failure", "Server didn't accept data:\n"+xhr.status+": "+xhr.responseText);
+				RTA.displayResponse("Failure", "Server didn't accept data:\n" + xhr.status + ": " + xhr.responseText);
 			}
 		} else if(xhr.readyState == 4 && xhr.status != 200) {
-			displayResponse("Failure", "Server responded with an irregular HTTP error code:\n"+xhr.status+": "+xhr.responseText);
+			RTA.displayResponse("Failure", "Server responded with an irregular HTTP error code:\n" + xhr.status + ": " + xhr.responseText);
 		}
 	};
 	
 	// mostly stolen from https://github.com/igstan/ajax-file-upload/blob/master/complex/uploader.js
-	var boundary = "AJAX-----------------------"+(new Date).getTime();
+	var boundary = "AJAX-----------------------" + (new Date).getTime();
 	
 	xhr.setRequestHeader("Content-Type", "multipart/form-data; boundary=" + boundary);
 	
@@ -43,7 +43,7 @@ function addTorrentTopyrtWebUI(data, filename) {
 	message += "Content-Disposition: form-data; name=\"start\"\r\n\r\n";
 	message += "on\r\n";
 	message += "--" + boundary + "\r\n";
-	message += "Content-Disposition: form-data; name=\"torrent\"; filename=\""+filename+"\"\r\n";
+	message += "Content-Disposition: form-data; name=\"torrent\"; filename=\"" + filename + "\"\r\n";
 	message += "Content-Type: application/x-bittorrent\r\n\r\n";
 	message += data + "\r\n";
 	message += "--" + boundary + "--\r\n";
--- webuiapis/qBittorrentWebUI.js
+++ webuiapis/qBittorrentWebUI.js
@@ -1,4 +1,4 @@
-function addTorrentToqBittorrentWebUI(data, torrentname) {
+RTA.clients.qBittorrentAdder = function(server, data, torrentname) {
 	var target;
 	if(data.substring(0,7) == "magnet:")
 		target = "download";
@@ -6,25 +6,25 @@ function addTorrentToqBittorrentWebUI(data, torrentname) {
 		target = "upload";
 	
 	var xhr = new XMLHttpRequest();
-	xhr.open("POST", "http"+((localStorage["hostsecure"]=='true')?"s":"")+"://"+localStorage["host"]+":"+localStorage["port"]+"/command/"+target, true, localStorage["login"], localStorage["password"]);
+	xhr.open("POST", "http" + ((server.hostsecure == 'true') ? "s" : "") + "://" + server.host + ":" + server.port + "/command/" + target, true, server.login, server.password);
 	xhr.onreadystatechange = function(data) {
 		if(xhr.readyState == 4 && xhr.status == 200) {
-			displayResponse("Success", "Torrent added successfully.");
+			RTA.displayResponse("Success", "Torrent added successfully.");
 		} else if(xhr.readyState == 4 && xhr.status != 200) {
-			displayResponse("Failure", "Server responded with an irregular HTTP error code:\n"+xhr.status+": "+xhr.responseText);
+			RTA.displayResponse("Failure", "Server responded with an irregular HTTP error code:\n" + xhr.status + ": " + xhr.responseText);
 		}
 	};
 	
 	if(data.substring(0,7) == "magnet:") {
 		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-		var message = "urls="+encodeURIComponent(data);
+		var message = "urls=" + encodeURIComponent(data);
 		xhr.send(message);
 	} else {
 		// mostly stolen from https://github.com/igstan/ajax-file-upload/blob/master/complex/uploader.js
-		var boundary = "AJAX-----------------------"+(new Date).getTime();
+		var boundary = "AJAX-----------------------" + (new Date).getTime();
 		xhr.setRequestHeader("Content-Type", "multipart/form-data; boundary=" + boundary);
 		var message = "--" + boundary + "\r\n";
-		   message += "Content-Disposition: form-data; name=\"torrentfile\"; filename=\""+((torrentname.length && torrentname.length>1) ? torrentname : (new Date).getTime())+"\"\r\n";
+		   message += "Content-Disposition: form-data; name=\"torrentfile\"; filename=\"" + ((torrentname.length && torrentname.length > 1) ? torrentname : (new Date).getTime()) + "\"\r\n";
 		   message += "Content-Type: application/x-bittorrent\r\n\r\n";
 		   message += data + "\r\n";
 		   message += "--" + boundary + "--\r\n";
