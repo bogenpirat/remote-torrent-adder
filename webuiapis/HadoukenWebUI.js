f0773d6a1941b66ba65e2fa5e231bdb6e21e5e22
julian <juliii@gmail.com>
2020-08-08 22:43:11 +0200

    hadouken: moved to fetch from XHR

--- webuiapis/HadoukenWebUI.js
+++ webuiapis/HadoukenWebUI.js
@@ -1,22 +1,6 @@
-RTA.clients.hadoukenAdder = function(server, torrentdata, name) {
-	var xhr = new XMLHttpRequest();
-	
-	xhr.open("POST", "http://" + server.host + ":" + server.port + "/api", false);
-	xhr.setRequestHeader("Authorization", "Basic " + btoa(server.login + ":" + server.password));
-	xhr.onreadystatechange = function(data) {
-		if(xhr.readyState == 4 && xhr.status == 200) {
-			if(JSON.parse(xhr.responseText).result == null) {
-				RTA.displayResponse("Success", "Torrent added successfully.");
-			} else {
-				RTA.displayResponse("Failure", "Server didn't accept data:\n" + xhr.status + " (" + xhr.statusText + "): " + xhr.responseText, true);
-			}
-		} else if(xhr.readyState == 4 && xhr.status != 200) {
-			RTA.displayResponse("Failure", "Server responded with an irregular HTTP error code:\n" + xhr.status + " (" + xhr.statusText + "): " + xhr.responseText, true);
-		}
-	};
-	
+RTA.clients.hadoukenAdder = async function(server, torrentdata, name) {
 	var message;
-	
+
 	if(torrentdata.substring(0,7) == "magnet:") {
 		message = JSON.stringify({
 			"id": 1,
@@ -31,18 +15,13 @@ RTA.clients.hadoukenAdder = function(server, torrentdata, name) {
 			]
 		});
 	} else {
-		// for proper base64 encoding, this needs to be shifted into a 8 byte integer array
-		var data = new ArrayBuffer(torrentdata.length);
-		var ui8a = new Uint8Array(data, 0);
-		for (var i=0; i<torrentdata.length; i++) {
-			ui8a[i] = (torrentdata.charCodeAt(i) & 0xff);
-		}
+		const data = new Uint8Array(await RTA.convertToBlob(torrentdata).arrayBuffer());
 		message = JSON.stringify({
 			"id": 1,
 			"jsonrpc": "2.0",
 			"method": "webui.addTorrent",
 			"params": [
-				b64_encode(ui8a),
+				b64_encode(data),
 				{
 					"savePath": server.hadoukendir == "" ? undefined : server.hadoukendir,
 					"label": server.hadoukenlabel
@@ -50,5 +29,20 @@ RTA.clients.hadoukenAdder = function(server, torrentdata, name) {
 			]
 		});
 	}
-	xhr.send(message)
-}
+
+	fetch("http://" + server.host + ":" + server.port + "/api", {
+		method: 'POST',
+		headers: {
+			"Authorization": "Basic " + btoa(server.login + ":" + server.password)
+		},
+		body: message
+	}).then(RTA.handleFetchError)
+	.then(response => response.json())
+	.then(json => {
+		RTA.displayResponse("Success", "Torrent added successfully to " + server.name + ".");
+	})
+	.catch(error => {
+		RTA.displayResponse("Failure", "Could not contact " + server.name + "\nError: " + error.message, true);
+	});
+
+};
