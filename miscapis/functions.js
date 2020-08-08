d1e829c816f80fb403066b08191c72bc5d538c30
julian <juliii@gmail.com>
2020-08-08 20:58:31 +0200

    created string->blob function to use for shaving off superfluous UTF-16 octet

--- miscapis/functions.js
+++ miscapis/functions.js
@@ -232,3 +232,14 @@ RTA.handleFetchError = function(response) {
 	}
 	return response;
 };
+
+
+RTA.convertToBlob = function(data, myType="text/plain") {
+	const ords = Array.prototype.map.call(data, function byteValue(x) {
+		return x.charCodeAt(0) & 0xff;
+	});
+	const ui8a = new Uint8Array(ords);
+	const dataBlob = new Blob([ui8a.buffer], {type: myType});
+
+	return dataBlob;
+};
