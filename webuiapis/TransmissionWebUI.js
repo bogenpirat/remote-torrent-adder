bfc14cb1e5c78899f1ca77756ba54f6271d85339
julian <juliii@gmail.com>
2020-08-09 02:49:01 +0200

    transmission: moved to fetch from XHR

--- webuiapis/TransmissionWebUI.js
+++ webuiapis/TransmissionWebUI.js
@@ -1,39 +1,52 @@
 RTA.clients.transmissionAdder = function(server, torrentdata) {
-	sendXHRTransmissionWebUI(server, torrentdata, "");
-}
+	const apiUrl = "http" + (server.hostsecure ? "s" : "") + "://" + server.host + ":" + server.port + "/transmission/rpc";
 
-function sendXHRTransmissionWebUI(server, torrentdata, sessionid) {
-	// for proper base64 encoding, this needs to be shifted into a 8 byte integer array
-	var data = new ArrayBuffer(torrentdata.length);
-	var ui8a = new Uint8Array(data, 0);
-	for (var i=0; i<torrentdata.length; i++) {
-		ui8a[i] = (torrentdata.charCodeAt(i) & 0xff);
-	}
-	
-	var message;
-	
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", "http" + (server.hostsecure ? "s" : "") + "://" + server.host + ":" + server.port + "/transmission/rpc", true);
-	xhr.setRequestHeader("Authorization", "Basic " + btoa(server.login + ":" + server.password));
-	xhr.setRequestHeader("X-Transmission-Session-Id", sessionid);
-	xhr.onreadystatechange = function(data) {
-		if(xhr.readyState == 4 && xhr.status == 200) {
-			if(/.*"result":"success".*/.exec(xhr.responseText)) {
-				RTA.displayResponse("Success", "Torrent added successfully.");
+	fetch(apiUrl, {
+		method: 'POST',
+		headers: {
+			"Authorization": "Basic " + btoa(server.login + ":" + server.password)
+		},
+		body: ""
+	})
+	.then(async response => {
+		if(response.status == 409) {
+			const sessionId = response.headers.get("X-Transmission-Session-Id");
+			const data = new Uint8Array(await RTA.convertToBlob(torrentdata).arrayBuffer());
+			
+			var message;
+			if(torrentdata.substring(0,7) == "magnet:") {
+				message = JSON.stringify({"method": "torrent-add", "arguments": {"paused": "false", "filename": torrentdata}});
 			} else {
-				RTA.displayResponse("Failure", "Server didn't accept data:\n" + xhr.status + ": " + xhr.responseText, true);
+				message = JSON.stringify({"method": "torrent-add", "arguments": {"metainfo": b64_encode(data)}});
 			}
-		} else if(xhr.readyState == 4 && xhr.status == 409) {
-			sendXHRTransmissionWebUI(server, torrentdata, xhr.getResponseHeader('X-Transmission-Session-Id'));
-		} else if(xhr.readyState == 4 && xhr.status != 200 && xhr.status != 409) {
-			RTA.displayResponse("Failure", "Server didn't accept data:\n" + xhr.status + ": " + xhr.responseText, true);
+			
+			fetch(apiUrl, {
+				method: 'POST',
+				headers: {
+					"Authorization": "Basic " + btoa(server.login + ":" + server.password),
+					"X-Transmission-Session-Id": sessionId,
+					"Content-Type": "application/json; charset=UTF-8"
+				},
+				body: message
+			})
+			.then(RTA.handleFetchError)
+			.then(response => response.json())
+			.then(json => {
+				if(json.result == "success") {
+					RTA.displayResponse("Success", "Torrent added successfully to " + server.name + ".");
+				} else {
+					RTA.displayResponse("Failure", "Adding the torrent failed:\n" + json.result , true);
+				}
+			})
+			.catch(error => {
+				RTA.displayResponse("Failure", "Could not contact " + server.name + "\nError: " + error.message, true);
+			});
+		} else {
+			throw new Error(response.statusText);
 		}
-	};
-	
-	if(torrentdata.substring(0,7) == "magnet:") {
-		message = JSON.stringify({"method": "torrent-add", "arguments": {"paused": "false", "filename": torrentdata}});
-	} else {
-		message = JSON.stringify({"method": "torrent-add", "arguments": {"metainfo": b64_encode(ui8a)}});
-	}
-	xhr.send(message);
-}
+	})
+	.catch(error => {
+		RTA.displayResponse("Failure", "Could not contact " + server.name + "\nError: " + error.message, true);
+	});
+
+};
