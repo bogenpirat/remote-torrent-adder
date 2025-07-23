4cb9db8891679a29a89ad9f212c726a152f87b00
julian <juliii@gmail.com>
2025-07-23 21:06:14 +0200

    fix webui auth listener

--- src/service_worker.ts
+++ src/service_worker.ts
@@ -3,11 +3,12 @@ import { WebUIFactory } from './models/clients';
 import { registerAuthenticationListenersForAllWebUis } from './util/authentication-listener';
 import { Settings } from './util/settings';
 
+
 const settingsProvider = new Settings();
 settingsProvider.loadSettings().then((settings) => {
     console.log("Settings loaded:", settings);
-    registerAuthenticationListenersForAllWebUis(settings.webuiSettings);
     const allWebUis = settings.webuiSettings.map(webUiSettings => WebUIFactory.createWebUI(webUiSettings)).filter(webUi => webUi !== null);
+    console.log("All WebUIs:", allWebUis);
+    registerAuthenticationListenersForAllWebUis(allWebUis);
     ContextMenu.createContextMenu(allWebUis);
 });
-
--- src/util/authentication-listener.ts
+++ src/util/authentication-listener.ts
@@ -1,41 +1,43 @@
-import { WebUISettings } from "../models/webui";
+import { TorrentWebUI } from "../models/webui";
 
-type OnAuthListenerCallback = (details: chrome.webRequest.OnAuthRequiredDetails, asyncCallback?: (response: chrome.webRequest.BlockingResponse) => void) => chrome.webRequest.BlockingResponse;
 
-const onAuthListeners: OnAuthListenerCallback[] = [];
+const onAuthListeners = [];
 var triedRequestIds: Set<string> = new Set();
 
-export function registerAuthenticationListenersForAllWebUis(allWebUiSettings: WebUISettings[]) {
-    while (onAuthListeners.length > 0) {
-        onAuthListeners.forEach(listener => {
-            chrome.webRequest.onAuthRequired.removeListener(listener);
-        });
-    }
+export function registerAuthenticationListenersForAllWebUis(allWebUis: TorrentWebUI[]) {
+    onAuthListeners.forEach(listener => {
+        chrome.webRequest.onAuthRequired.removeListener(listener);
+    });
+    onAuthListeners.length = 0;
 
-    allWebUiSettings.forEach(webUiSettings => {
-        const url = "http" + (webUiSettings.secure ? "s" : "") + "://" + webUiSettings.host + ":" + webUiSettings.port + "/";
+    allWebUis.forEach(webUi => {
+        const url = webUi.createBaseUrl();
 
-        const listener: OnAuthListenerCallback = (function (user: string, pass: string, url: string) {
-            return function (details: chrome.webRequest.OnAuthRequiredDetails) {
-                let suppliedAuthCredentials: chrome.webRequest.BlockingResponse = {};
+        const listener = function (
+            details: chrome.webRequest.OnAuthRequiredDetails
+        ): chrome.webRequest.BlockingResponse {
+            if (details.tabId !== -1) {
+                return {};
+            }
 
-                if (triedRequestIds.has(details.requestId)) { // second firing of an auth event for this request - let the browser handle it
-                    triedRequestIds.delete(details.requestId);
-                } else if (details.tabId != -1) {
-                    // do not handle auth requests for tabs
-                } else {
-                    suppliedAuthCredentials = { authCredentials: { username: user, password: pass } };
-                    triedRequestIds.add(details.requestId);
+            if (triedRequestIds.has(details.requestId)) {
+                // Second firing for this request, let the browser handle it
+                triedRequestIds.delete(details.requestId);
+                return {};
+            }
+
+            triedRequestIds.add(details.requestId);
+            return {
+                authCredentials: {
+                    username: webUi.settings.username,
+                    password: webUi.settings.password
                 }
-
-                return suppliedAuthCredentials;
             };
-        })(webUiSettings.username, webUiSettings.password, url);
+        };
 
-        if (webUiSettings.host && webUiSettings.port) {
-            chrome.webRequest.onAuthRequired.addListener(listener, { urls: [url + "*"], tabId: -1 }, ["blocking"]);
+        if (webUi.settings.host && webUi.settings.port) {
+            chrome.webRequest.onAuthRequired.addListener(listener, { urls: [`${url}*`] }, ["blocking"]);
+            onAuthListeners.push(listener);
         }
-
-        onAuthListeners.push(listener);
     });
 }
