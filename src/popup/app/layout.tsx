4bee00c3be6f8f43c98ed9381e55b10ac6712856
julian <juliii@gmail.com>
2025-08-02 18:14:06 +0200

    cleanup

--- src/popup/app/layout.tsx
+++
@@ -1,6 +0,0 @@
-import './globals.css';
-import React from 'react';
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div className="h-full font-sans">{children}</div>;
-}
