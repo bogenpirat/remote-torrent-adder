0adab9a59b4053347c42a5a0012708d65708f6f3
julian <juliii@gmail.com>
2025-09-04 09:15:44 +0200

    action icon: always enabled instead of disabled when no links available #379

--- src/util/action.ts
+++ src/util/action.ts
@@ -16,13 +16,6 @@ export function registerClickActionForIcon(webUi: TorrentWebUI | null): (tab: ch
 
 export function updateBadgeText(text: string, tabId: number): void {
     if (text !== '') {
-        chrome.action.enable(tabId);
         chrome.action.setBadgeText({ text, tabId });
-    } else {
-        chrome.action.getBadgeText({ tabId }).then(badgeText => {
-            if (!badgeText) {
-                chrome.action.disable(tabId);
-            }
-        });
     }
 }
